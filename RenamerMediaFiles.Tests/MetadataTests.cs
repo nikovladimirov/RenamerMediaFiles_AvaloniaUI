9964e38616ca38485484535f63b949ece9095c7a
Nikolai Vladimirov <vladimirov@mzta.ru>
2023-07-01 21:41:18 +0300

    updated gopro test file

--- RenamerMediaFiles.Tests/MetadataTests.cs
+++ RenamerMediaFiles.Tests/MetadataTests.cs
@@ -33,9 +33,8 @@ public class MetadataTests
     [InlineData("./TestFiles/apple/20230630_152919.mov",4,"2023.06.30 15:29:19")]
     [InlineData("./TestFiles/samsung/20230630_153036.mp4",4,"2023.06.30 15:30:36")]
     [InlineData("./TestFiles/huawei/20230630_152820.mp4",4,"2023.06.30 15:28:20")]
-    [InlineData("./TestFiles/gopro/20230630_154202.mp4",4,"2023.06.30 15:42:02")]
-    [InlineData("./TestFiles/gopro/20230630_154148.MP4",4,"2023.06.30 15:41:48")]
-    [InlineData("./TestFiles/gopro/2022-08-23 111241.mp4",3,"2022.08.23 11:12:41")]
+    [InlineData("./TestFiles/gopro/20230701_223321.MP4",4,"2023.07.01 22:33:21")]
+    
     public void CheckParsing_QuickTimeMovieHeader_DateTime(string filePath,float offserHour, string expectedDateString)
     {
         var fileInfo = new FileInfo(filePath);
@@ -48,9 +47,7 @@ public class MetadataTests
     [InlineData("./TestFiles/apple/20230630_152919.mov",4,"2023.06.30 15:29:19")]
     [InlineData("./TestFiles/samsung/20230630_153036.mp4",4,"2023.06.30 15:30:36")]
     [InlineData("./TestFiles/huawei/20230630_152820.mp4",4,"2023.06.30 15:28:20")]
-    [InlineData("./TestFiles/gopro/20230630_154202.mp4",4,"2023.06.30 15:42:02")]
-    [InlineData("./TestFiles/gopro/20230630_154148.MP4",4,"2023.06.30 15:41:48")]
-    [InlineData("./TestFiles/gopro/2022-08-23 111241.mp4",3,"2022.08.23 11:12:41")]
+    [InlineData("./TestFiles/gopro/20230701_223321.MP4",4,"2023.07.01 22:33:21")]
     public void CheckParsing_QuickTimeTrackHeader_DateTime(string filePath, float offserHour, string expectedDateString)
     {
         var fileInfo = new FileInfo(filePath);
@@ -70,5 +67,4 @@ public class MetadataTests
         
         Assert.Equal(expectedDate, result.Value);
     }
-    
 }
